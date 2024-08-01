4d9ab98b90bc6de2e4c2443be47f293b656c61cb
Myron Scott <myron.scott@softwareag.com>
2024-08-01 10:11:26 -0700

    Update to Apache License v2 with IBM copyright

--- tc-config-parser/src/main/java/org/terracotta/config/TCConfigurationParser.java
+++ tc-config-parser/src/main/java/org/terracotta/config/TCConfigurationParser.java
@@ -1,21 +1,19 @@
 /*
- * The contents of this file are subject to the Terracotta Public License Version
- * 2.0 (the "License"); You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * Copyright Terracotta, Inc.
+ * Copyright Super iPaaS Integration LLC, an IBM Company 2024
  *
- * http://terracotta.org/legal/terracotta-public-license.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The Covered Software is Terracotta Configuration.
- *
- * The Initial Developer of the Covered Software is
- * Terracotta, Inc., a Software AG company
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.terracotta.config;
 
 
@@ -47,6 +45,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -259,6 +258,7 @@ public class TCConfigurationParser {
     return parse(xmlText, Thread.currentThread().getContextClassLoader());
   }
 
+  @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DM_DEFAULT_ENCODING")
   public static TcConfiguration parse(String xmlText, ClassLoader loader) throws IOException, SAXException {
     return convert(new ByteArrayInputStream(xmlText.getBytes()), null, loader);
   }
